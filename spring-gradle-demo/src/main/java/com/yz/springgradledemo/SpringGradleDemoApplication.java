92f6a82f10e33a8607e5cbd56b97cce332205d1b
yzhang33 <youmingzhang19@gmail.com>
2023-08-12 10:24:25 -0400

    added gradle build set up

---
+++ spring-gradle-demo/src/main/java/com/yz/springgradledemo/SpringGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yz.springgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleDemoApplication.class, args);
+	}
+
+}
